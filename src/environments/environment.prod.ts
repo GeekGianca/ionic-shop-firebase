8c15cf74864f86640013d20395c58a444b0e70c4
Gian Carlos Cuello Calle <giancarlos.cuello@gmail.com>
2019-12-16 18:42:57 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAWoguBp8JLD8KE93f8sZo-R_hoQETNvv4",
-    authDomain: "surf-tienda.firebaseapp.com",
-    databaseURL: "https://surf-tienda.firebaseio.com",
-    projectId: "surf-tienda",
-    storageBucket: "surf-tienda.appspot.com",
-    messagingSenderId: "672967003127",
-    appId: "1:672967003127:web:d34e9aaf314bb15f1b3cf2",
-    measurementId: "G-JV9449B1VE"
-  }
-};
+export const environment = {};
