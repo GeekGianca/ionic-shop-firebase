aed5104c02880a085753b6bbc5597766178ceddc
Gian Carlos Cuello Calle <giancarlos.cuello@gmail.com>
2019-12-16 18:43:21 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyAWoguBp8JLD8KE93f8sZo-R_hoQETNvv4",
-    authDomain: "surf-tienda.firebaseapp.com",
-    databaseURL: "https://surf-tienda.firebaseio.com",
-    projectId: "surf-tienda",
-    storageBucket: "surf-tienda.appspot.com",
-    messagingSenderId: "672967003127",
-    appId: "1:672967003127:web:d34e9aaf314bb15f1b3cf2",
-    measurementId: "G-JV9449B1VE"
-  }
-};
+export const environment = {};
 
 /*
  * For easier debugging in development mode, you can import the following file
